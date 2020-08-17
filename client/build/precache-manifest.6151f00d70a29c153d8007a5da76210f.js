f71431d1964e5d83e625afa8147b36c93b4c9bc6
Sushmitha Kothuru <skothuru@asu.edu>
2020-08-17 11:36:57 -0700

    initial commit

---
+++ client/build/precache-manifest.6151f00d70a29c153d8007a5da76210f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d34c50b031b4a58b212c0191591a444",
+    "url": "/index.html"
+  },
+  {
+    "revision": "359d4ee4de4978e76e07",
+    "url": "/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359d4ee4de4978e76e07",
+    "url": "/static/js/main.f2e84616.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
